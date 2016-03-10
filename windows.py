2c62758f12c5605dc854dc16d17908df1e234d93
Kaspars Mickevics <kaspars@fx.lv>
2016-03-10 10:12:09 +0100

    Battery status added

--- windows.py
+++ windows.py
@@ -12,10 +12,13 @@ if len(perf) == 1:
 	perf = perf[0]
 	
 if __name__ == "__main__":
-	print "OS data"
-	print os
-	print "Performance data"
-	print perf
+    print "OS data"
+    print os
+    print "Performance data"
+    print perf
+    batteries = w.query('select * from Win32_Battery')
+    for battery in batteries:
+        print "Battery '{}', status: {}, charge remaining: {}%".format(battery.Caption, battery.Status, battery.EstimatedChargeRemaining)
 else:
 	print "Windows.py loaded"
 	print "windows.os object == Win32_OperatingSystem()"
