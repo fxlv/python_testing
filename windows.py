a55dff024c38b851c659bb37bd254eace3555526
Kaspars Mickevics <kaspars@fx.lv>
2016-03-13 13:34:34 +0200

    style improvements

--- windows.py
+++ windows.py
@@ -5,12 +5,12 @@ w = wmi.WMI()
 
 os = w.Win32_OperatingSystem()
 if len(os) == 1:
-	os = os[0]
+    os = os[0]
 
 perf = w.Win32_PerfFormattedData_PerfOS_System()
 if len(perf) == 1:
-	perf = perf[0]
-	
+    perf = perf[0]
+
 if __name__ == "__main__":
     print "OS data"
     print os
@@ -18,8 +18,9 @@ if __name__ == "__main__":
     print perf
     batteries = w.query('select * from Win32_Battery')
     for battery in batteries:
-        print "Battery '{}', status: {}, charge remaining: {}%".format(battery.Caption, battery.Status, battery.EstimatedChargeRemaining)
+        print "Battery '{}', status: {}, charge remaining: {}%".format(
+            battery.Caption, battery.Status, battery.EstimatedChargeRemaining)
 else:
-	print "Windows.py loaded"
-	print "windows.os object == Win32_OperatingSystem()"
-	print "windows.perf object == Win32_PerfFormattedData_PerfOS_System()"
+    print "Windows.py loaded"
+    print "windows.os object == Win32_OperatingSystem()"
+    print "windows.perf object == Win32_PerfFormattedData_PerfOS_System()"
